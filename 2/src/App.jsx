a90a252d4ff03f843fe3d59c16914810336938d1
Sameer <sameer.marathe16536@sakec.ac.in>
2024-06-15 23:22:14 +0530

    useNavigate()

--- 2/src/App.jsx
+++ 2/src/App.jsx
@@ -2,26 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import Landing from './components/Landing'
 import Dashboard from './components/Dashboard'
 
 function App() {
-
+  
 
   return (
 
+
     <div>
-      <div style={{ backgroundColor: "blue" }}>
-        {/* <button onClick={()=>{
-          window.location.href="/";
-        }}>Home</button>
-        <button onClick={()=>{
-          window.location.href="/dashboard";
-        }}>Dashboard</button> */}
-      </div>
+      
       <div>
         <BrowserRouter>
+        <AppBar/>
           <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/dashboard" element={<Dashboard />} />
@@ -33,4 +28,22 @@ function App() {
   )
 }
 
+const AppBar =()=>{
+  const navigate = useNavigate()
+  return(
+    <div>
+      <div style={{ backgroundColor: "blue" }}>
+        <button onClick={()=>{
+          // window.location.href="/";
+          navigate("/")
+        }}>Home</button>
+        <button onClick={()=>{
+          // window.location.href="/dashboard";
+          navigate("/dashboard")
+        }}>Dashboard</button>
+      </div>
+      
+    </div>
+  )
+}
 export default App
--- 2/src/components/Dashboard.jsx
+++ 2/src/components/Dashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const handleClick =()=>{
+    navigate('/')
+  }
   return (
-    <div>Dashboard</div>
+    <div>Dashboard
+      <button onClick={handleClick}>Click to Navigate</button>
+    </div>
   )
 }
 
