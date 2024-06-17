b23a42e39b129817dae9f324ae089dc99518e709
Sameer <sameer.marathe16536@sakec.ac.in>
2024-06-17 19:36:41 +0530

    ass 2

--- assignments/ass2/src/App.jsx
+++ assignments/ass2/src/App.jsx
@@ -4,31 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[bgColor,setBgColor]=useState('white')
+
+  const colorChanger =(color)=>{
+    setBgColor(color)
+  } 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='background-changer' style={{backgroundColor: bgColor}}>
+      <button className="color-button red" onClick={()=>{colorChanger('red')}}>Red</button>
+      <button className="color-button yellow" onClick={()=>{colorChanger('yellow')}}>Yellow</button>
+      <button className="color-button black" onClick={()=>{colorChanger('black')}}>Black</button>
+      <button className="color-button purple" onClick={()=>{colorChanger('purple')}}>Purple</button>
+      <button className="color-button blue" onClick={()=>{colorChanger('blue')}}>Blue</button>
+      <button className="color-button green" onClick={()=>{colorChanger('green')}}>Green</button>
+      <button className="color-button default" onClick={()=>{colorChanger('white')}}>Default</button>
+    </div>
   )
 }
 
